686b292082f8f6b2269778e566bfbd20dedceacc
11Manub11 <manuelbarrios@uma.es>
2024-10-17 19:24:38 +0200

    retoquillos

--- WEB/web_script.js
+++ WEB/web_script.js
@@ -8,10 +8,6 @@
     }
 
     numberBox.oninput = function() {
-        // if (this.value < 5 && this.value != "")
-        //     this.value = 5;
-        // else if (this.value > 1000)
-        //     this.value = 1000;
         slider.value = this.value;
         addCircle(document.getElementById('latitud').value, document.getElementById('longitud').value);
     }
