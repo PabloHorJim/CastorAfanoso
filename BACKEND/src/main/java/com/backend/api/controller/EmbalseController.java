d152cd6a513db27333d3a6536004ba4d8bdbae76
Eduardo Cantero <eduardojesuscantero04@gmail.com>
2024-10-17 13:41:00 +0200

    backen

--- BACKEND/src/main/java/com/backend/api/controller/EmbalseController.java
+++ BACKEND/src/main/java/com/backend/api/controller/EmbalseController.java
@@ -1,17 +1,24 @@
 package com.backend.api.controller;
 
-import com.backend.service.pantanoService;
+import com.backend.api.model.Embalse;
+import com.backend.service.EmbalseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmbalseController {
-    pantanoService service;
+    EmbalseService service;
 
     @Autowired
-    public EmbalseController(pantanoService service) {
+    public EmbalseController(EmbalseService service) {
         this.service = service;
     }
 
-
+    @GetMapping("/")
+    public Embalse getPantanos(@RequestParam String x, String y, Integer dist){
+        Optional<User> user = userService.getUserID(id);
+        return user.isPresent()? user.get() : null;
+    }
 }
--- BACKEND/src/main/java/com/backend/service/EmbalseService.java
+++ BACKEND/src/main/java/com/backend/service/EmbalseService.java
@@ -1,4 +1,4 @@
 package com.backend.service;
 
-public class pantanoService {
+public class EmbalseService {
 }
