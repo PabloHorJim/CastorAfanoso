fe8c4950bf3736f5609ab923027139acf06bcb44
Eduardo Cantero <eduardojesuscantero04@gmail.com>
2024-10-17 15:00:08 +0200

    embalseController

--- BACKEND/src/main/java/com/backend/api/controller/EmbalseController.java
+++ BACKEND/src/main/java/com/backend/api/controller/EmbalseController.java
@@ -3,22 +3,36 @@ package com.backend.api.controller;
 import com.backend.api.model.Embalse;
 import com.backend.service.EmbalseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class EmbalseController {
     EmbalseService service;
 
     @Autowired
-    public EmbalseController(EmbalseService service) {
+    public EmbalseController(EmbalseService service){
         this.service = service;
     }
 
+    /**
+     * URL para la peticion http://localhost:8080/?x=valorX&y=valorY&dist=dist
+     * @param x
+     * @param y
+     * @param dist
+     * @return
+     */
     @GetMapping("/")
-    public Embalse getPantanos(@RequestParam String x, String y, Integer dist){
-        Optional<User> user = userService.getUserID(id);
-        return user.isPresent()? user.get() : null;
+    public ResponseEntity<List<Embalse>> getPantanos(@RequestParam String x, String y, Integer dist){
+        List<Embalse> embalses = EmbalseService.getFromDist(x,y,dist);
+        if (embalses.isEmpty())
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        return ResponseEntity.ok(embalses);
     }
 }
