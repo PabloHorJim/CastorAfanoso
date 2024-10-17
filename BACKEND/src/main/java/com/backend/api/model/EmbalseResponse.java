21830e6e8a77b567812377f91bae34efad622858
PabloHorJim <pablohorjim@uma.es>
2024-10-17 17:22:04 +0200

    Funcional communication with DB. Only some embalses are retrieved, but is something

---
+++ BACKEND/src/main/java/com/backend/api/model/EmbalseResponse.java
@@ -0,0 +1,15 @@
+package com.backend.api.model;
+
+import java.util.List;
+
+public class EmbalseResponse {
+    private List<Embalse> items;
+
+    public List<Embalse> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Embalse> items) {
+        this.items = items;
+    }
+}
