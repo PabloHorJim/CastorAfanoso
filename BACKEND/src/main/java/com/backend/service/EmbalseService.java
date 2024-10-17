f8d4ac6678d2d26a7b450858d556fe00d96fe4b4
PabloHorJim <pablohorjim@uma.es>
2024-10-17 17:08:18 +0200

    Added https connection with the data (not functional)

--- BACKEND/src/main/java/com/backend/service/EmbalseService.java
+++ BACKEND/src/main/java/com/backend/service/EmbalseService.java
@@ -3,16 +3,24 @@ package com.backend.service;
 import com.backend.api.model.Embalse;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import com.google.gson.Gson;
 
+import javax.net.ssl.HttpsURLConnection;
+
 @Service
 public class EmbalseService {
+    private static String app_name = "Castores Afanosos";
+    private static int year = 2024;
 
     private static List<Embalse> embalses = new ArrayList<Embalse>();
-    private static String enlaceBD = "" ;
+    private static String enlaceBD = "https://gbdc01ffb7d1854-castoresafanosos.adb.eu-madrid-1.oraclecloudapps.com/ords/castor_afanoso/combined_embalses/" ;
 
     public EmbalseService() {
         Embalse aux = new Embalse(3,
@@ -51,7 +59,7 @@ public class EmbalseService {
         return R * c; // Distancia en km
     }
 
-    public static double calcularDistancia(String latitudA, String longitudA, String latitudB, String longitudB) {
+    private static double calcularDistancia(String latitudA, String longitudA, String latitudB, String longitudB) {
         try {
             // Reemplazar coma por punto
             latitudA = latitudA.replace(",", ".");
@@ -80,5 +88,54 @@ public class EmbalseService {
                 embalsesAux.add(embalse);
         }
         return embalsesAux;
+    }
+
+    private HttpsURLConnection getHttpsURLConnection(String url_resource) throws IOException {
+
+        URL service = new URL(url_resource);
+        // Create the connection from the URL
+        HttpsURLConnection connection = (HttpsURLConnection) service.openConnection();
+        // Add the headers User-Agent and Accept (see the statement)
+        connection.setRequestProperty("User-Agent", app_name + "-" + year);
+        connection.setRequestProperty("Accept", "application/json");
+        // Indicate that it is a GET request
+        connection.setRequestMethod("GET");
+        // Check that the response code received is correct
+        int responseCode = connection.getResponseCode();
+        if (responseCode < 200 || responseCode >= 300) {
+            throw new IOException("Failed trying to access: " + url_resource + "\nHTTP error code : " + responseCode);
+        }
+        return connection;
+    }
+
+    /**
+     * This method is a generic method that deserializes the response to the specified class.
+     * @param connection The connection to the resource
+     * @param tClass The class to which the response will be deserialized (cannot infer the class from T, as it is erased in Runtime)
+     * @return The object of type T deserialized from the response
+     * @param <T> The type of the object to be deserialized
+     * @throws IOException If there is an error deserializing the response
+     */
+    private <T> T deserializeResponse(HttpsURLConnection connection, Class<T> tClass) throws IOException {
+        Gson parser = new Gson();
+        InputStream in = connection.getInputStream(); // Get the InputStream from the connection
+        return parser.fromJson(new InputStreamReader(in), tClass);
+    }
+
+    public <T> T getResource(String urlname, Class<T> tClass) {
+        urlname = urlname.replaceAll("http:", "https:");
+        try {
+            HttpsURLConnection connection = getHttpsURLConnection(urlname);
+            return deserializeResponse(connection, tClass);
+        } catch (IOException e) {
+            //If the resource is not found, the upper level will retry with another resource
+            return null;
+        }
+    }
+
+    private void printEmbalses(List<Embalse> embalses) {
+        for (Embalse embalse : embalses) {
+            System.out.println(embalse.toString());
+        }
     }
 }
