114754ffe87e355ae14dfed36cfda8ee9b8d59a9
11Manub11 <manuelbarrios@uma.es>
2024-10-17 17:14:08 +0200

    Added load screen and better map

--- WEB/location_script.js
+++ WEB/location_script.js
@@ -1,19 +1,25 @@
 document.getElementById("getLocation").addEventListener("click", 
     function() {
-        document.getElementById("latitud").value = 0;
-        document.getElementById("longitud").value = 0;
-        document.getElementById("radar_loader").style.display = "flex";
+        document.getElementById("loader").style.display = "flex";
+        document.getElementById("latitud").disabled = true;
+        document.getElementById("longitud").disabled = true;
         document.getElementById("getLocation").style.display = "none";
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition, showError);
-    } else {
-        alert("Geolocation is not supported by this browser.");
-    }
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition, showError);
+        } else {
+            alert("Geolocation is not supported by this browser.");
+        }
 });
 
 function showPosition(position) {
+    document.getElementById("latitud").disabled = false;
+    document.getElementById("longitud").disabled = false;
+
     document.getElementById("latitud").value = position.coords.latitude;
     document.getElementById("longitud").value = position.coords.longitude;
+    
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("getLocation").style.display = "flex";
 }
 
 function showError(error) {
